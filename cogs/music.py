51924893450408f5393e27d2c0328fe66a79148c
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2020-11-30 18:18:56 +0530

    opus

--- cogs/music.py
+++ cogs/music.py
@@ -9,8 +9,20 @@ from async_timeout import timeout
 from functools import partial
 from youtube_dl import YoutubeDL
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+import ctypes
+import ctypes.util
+
+print("ctypes - Find opus:")
+a = ctypes.util.find_library('opus')
+print(a)
+
+print("Discord - Load Opus:")
+b = discord.opus.load_opus(a)
+print(b)
+
+print("Discord - Is loaded:")
+c = discord.opus.is_loaded()
+print(c)
 
 ytdlopts = {
     'format': 'bestaudio/best',
